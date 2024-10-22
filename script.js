4fd7d47864afbe22c45fcdab04e6f4df6b2e67ea
C.Vignesh <162482102+VIGNESH1280@users.noreply.github.com>
2024-10-22 19:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+ async function vignesh(){
+	 let arr=[1,2,3,4];
+	 return arr;
+ }
+vignesh().then((arr)=>{
+	let evens = arr.map((a)=>{
+		return a%2==0
+	})
+	setTimeout(()={
+		let el = document.getElementById('output');
+		el.innerHTML=evens.join();
+	}, 1000)
+	return evens;
+}).then((evens)=>{
+	let mulEven = evens.map((value)=>{
+		return value*2;
+	})
+	setTimeout(()={
+		let el = document.getElementById('output');
+		el.innerHTML=mulEven.join();
+	}, 2000)
+	
+})
